1a82c7265e2fe6386f963fddeb52bdebe20cb610
AdiyoFouad <odjfouad38@gmail.com>
2025-11-15 08:43:00 +0100

    Configuration pour le déploiement

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def load_user(user_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
