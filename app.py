0e78a839f21d4fce70196d6fefbbb7f035ff137b
AdiyoFouad <odjfouad38@gmail.com>
2025-11-15 08:05:07 +0100

    Configuration pour le déploiement

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask_jwt_extended import JWTManager
 from routes import  cocktail_blueprint, main_blueprint, user_blueprint
 from database import db
 from models import User
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fouad:adiyo@localhost/cocktailgenius'
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://fouad:adiyo@localhost/cocktailgenius')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','your_secret_key')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY','jwt_secret_key')
 
 db.init_app(app)
 jwt = JWTManager(app)
@@ -33,4 +34,4 @@ def load_user(user_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(port=int(os.environ.get("PORT", 5000)), debug=True)
